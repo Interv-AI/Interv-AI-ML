8c84ee2f160d81b5ea331bc11b27731bc9dbe848
Gaurav Misal <91176604+Garry028@users.noreply.github.com>
2024-01-22 21:49:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 import spacy
 nlp = spacy.load("en_core_web_md")
 
-
+app=Flask(__name__)
 @app.route('/calculate_similarity', methods=['POST'])
 def calculate_similarity():
     actual_answer = request.form['actual_answer']
@@ -16,3 +16,6 @@ def calculate_similarity():
         [actual_embedding], [targeted_embedding])[0, 0]
 
     return jsonify({'similarity_score': similarity_score})
+
+if __name__=='__main__':
+    app.run()
