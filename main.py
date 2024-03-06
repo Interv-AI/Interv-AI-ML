899e5472fd9f15ddd112def3aa3f32b8f2fb63d0
NikhilBerwal <90278400+NikhilBerwal@users.noreply.github.com>
2024-03-06 08:58:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-from sklearn.metrics.pairwise import cosine_similarity
+#install- pip install sentence-transformers
 from flask import Flask, request, jsonify
-import spacy
-nlp = spacy.load("en_core_web_md")
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+#all_MiniLM-L6v2-pre-trained BERT model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
 @app.route('/calculate_similarity', methods=['POST'])
 def calculate_similarity():
-    actual_answer = request.form['actual_answer']
-    targeted_answer = request.form['targeted_answer']
+    data = request.get_json() 
 
-    actual_embedding = nlp(actual_answer).vector
-    targeted_embedding = nlp(targeted_answer).vector
+    actual_answer = data['actual_answer']
+    targeted_answer = data['targeted_answer']
 
+    # Encode the texts to get the embeddings
+    embeddings = model.encode([actual_answer, targeted_answer])
+
+    # Calculate cosine similarity
     similarity_score = cosine_similarity(
-        [actual_embedding], [targeted_embedding])[0, 0]
+        [embeddings[0]], [embeddings[1]])[0, 0]
 
     return jsonify({'similarity_score': similarity_score})
+
 
 # if __name__=='__main__':
 #     app.run()
