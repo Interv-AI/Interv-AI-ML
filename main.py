8a8a8844ddfd5ce6a74e67eadcf863bae450c0e2
NikhilBerwal <90278400+NikhilBerwal@users.noreply.github.com>
2024-03-06 09:26:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#install- pip install sentence-transformers
 from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,20 +10,22 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 @app.route('/calculate_similarity', methods=['POST'])
 def calculate_similarity():
-    data = request.get_json() 
+    data = request.get_json()
 
-    actual_answer = data['actual_answer']
-    targeted_answer = data['targeted_answer']
+    actual_answer = data.get("actual_answer")
+    targeted_answer = data.get("targeted_answer")
+        
 
     # Encode the texts to get the embeddings
     embeddings = model.encode([actual_answer, targeted_answer])
 
     # Calculate cosine similarity
-    similarity_score = cosine_similarity(
-        [embeddings[0]], [embeddings[1]])[0, 0]
+    similarity_score = cosine_similarity([embeddings[0]], [embeddings[1]])[0, 0]
+
+    similarity_score = np.float64(similarity_score)
 
     return jsonify({'similarity_score': similarity_score})
 
 
-# if __name__=='__main__':
-#     app.run()
+#if __name__ == '__main__':
+    #app.run(debug=True)
