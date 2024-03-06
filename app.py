3499de2d6d68ac962d7e424adb4e92e2f726f5a5
Garry <gmisal2002@gmail.com>
2024-03-06 12:10:37 +0530

    fixes

--- app.py
+++ app.py
@@ -5,27 +5,34 @@ import numpy as np
 
 app = Flask(__name__)
 
-#all_MiniLM-L6v2-pre-trained BERT model
+# all_MiniLM-L6v2-pre-trained BERT model
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
+
 @app.route('/calculate_similarity', methods=['POST'])
+@app.route('/', methods=['GET'])
 def calculate_similarity():
     data = request.get_json()
 
     actual_answer = data.get("actual_answer")
     targeted_answer = data.get("targeted_answer")
 
-
     # Encode the texts to get the embeddings
     embeddings = model.encode([actual_answer, targeted_answer])
 
     # Calculate cosine similarity
-    similarity_score = cosine_similarity([embeddings[0]], [embeddings[1]])[0, 0]
+    similarity_score = cosine_similarity(
+        [embeddings[0]], [embeddings[1]])[0, 0]
 
     similarity_score = np.float64(similarity_score)
 
     return jsonify({'similarity_score': similarity_score})
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'status': 'Server is running'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
