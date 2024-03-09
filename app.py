e229150c0102742f52f39dbcbb3e965244578b71
Garry <gmisal2002@gmail.com>
2024-03-09 12:17:10 +0530

    made changes

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask, request, jsonify
-from sentence_transformers import SentenceTransformer
+# from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+import spacy
 
 app = Flask(__name__)
 
-# all_MiniLM-L6v2-pre-trained BERT model
-model = SentenceTransformer('all-MiniLM-L6-v2')
+# # all_MiniLM-L6v2-pre-trained BERT model
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+# @app.route('/calculate_similarity', methods=['POST'])
+# def calculate_similarity():
+#     data = request.get_json()
+
+#     actual_answer = data.get("actual_answer")
+#     targeted_answer = data.get("targeted_answer")
+
+#     # Encode the texts to get the embeddings
+#     embeddings = model.encode([actual_answer, targeted_answer])
+
+#     # Calculate cosine similarity
+#     similarity_score = cosine_similarity(
+#         [embeddings[0]], [embeddings[1]])[0, 0]
+
+#     similarity_score = np.float64(similarity_score)
+
+#     return jsonify({'similarity_score': similarity_score})
+
+
+nlp = spacy.load("en_core_web_md")
 
 
 @app.route('/calculate_similarity', methods=['POST'])
@@ -16,14 +39,11 @@ def calculate_similarity():
     actual_answer = data.get("actual_answer")
     targeted_answer = data.get("targeted_answer")
 
-    # Encode the texts to get the embeddings
-    embeddings = model.encode([actual_answer, targeted_answer])
+    actual_embedding = nlp(actual_answer).vector
+    targeted_embedding = nlp(targeted_answer).vector
 
-    # Calculate cosine similarity
     similarity_score = cosine_similarity(
-        [embeddings[0]], [embeddings[1]])[0, 0]
-
-    similarity_score = np.float64(similarity_score)
+        [actual_embedding], [targeted_embedding])[0, 0]
 
     return jsonify({'similarity_score': similarity_score})
 
