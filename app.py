de61d3571ff897096f4feed3ec7f48b60dc6051d
Garry <gmisal2002@gmail.com>
2024-03-09 12:42:22 +0530

    changed model

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
-# from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer, util
+# from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-import spacy
+# import spacy
 
 app = Flask(__name__)
 
@@ -29,8 +29,26 @@ app = Flask(__name__)
 #     return jsonify({'similarity_score': similarity_score})
 
 
-nlp = spacy.load("en_core_web_md")
+# nlp = spacy.load("en_core_web_md")
 
+# @app.route('/calculate_similarity', methods=['POST'])
+# def calculate_similarity():
+#     data = request.get_json()
+
+#     actual_answer = data.get("actual_answer")
+#     targeted_answer = data.get("targeted_answer")
+
+#     actual_embedding = nlp(actual_answer).vector
+#     targeted_embedding = nlp(targeted_answer).vector
+
+#     similarity_score = cosine_similarity(
+#         [actual_embedding], [targeted_embedding])[0, 0]
+
+#     return jsonify({'similarity_score': similarity_score})
+
+
+# Load DistilBERT model
+model = SentenceTransformer('distilbert-base-nli-stsb')
 
 @app.route('/calculate_similarity', methods=['POST'])
 def calculate_similarity():
@@ -39,25 +57,19 @@ def calculate_similarity():
     actual_answer = data.get("actual_answer")
     targeted_answer = data.get("targeted_answer")
 
-    actual_embedding = nlp(actual_answer).vector
-    targeted_embedding = nlp(targeted_answer).vector
+    # Encode the texts to get the embeddings
+    embeddings = model.encode([actual_answer, targeted_answer])
 
-    similarity_score = cosine_similarity(
-        [actual_embedding], [targeted_embedding])[0, 0]
+    # Calculate cosine similarity
+    similarity_score = util.pytorch_cos_sim(embeddings[0], embeddings[1]).item()
+
+    similarity_score = np.float64(similarity_score)
 
     return jsonify({'similarity_score': similarity_score})
 
-
-@app.route('/hello', methods=['POST'])
-def hello():
-    data = request.get_json()
-    a = data.get("hi")
-    return jsonify({'similarity_score': a})
-
-
 @app.route('/')
 def home():
-    return 'Server is running'
+    return 'Server is running 🎊🔥'
 
 
 if __name__ == '__main__':
